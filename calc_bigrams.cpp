007f6b3da47c2b89b4b14c6a2c1c2c2b7079668f
Flo Salinas <florenciabsalinas@gmail.com>
2016-06-14 01:04:46 -0400

    seperate into new play fct

--- calc_bigrams.cpp
+++ calc_bigrams.cpp
@@ -13,12 +13,15 @@
 
 
 template <typename T>
-void printDict(std::map<T,int>);
+void printDict(std::map<T,int>&);
 
 template <typename T>
-void calcBigramProb(std::map<T,int>& dictionary, std::map<std::pair<T, T>,int>& pairDict, std::pair<T,T> input);
+void calcBigramProb(std::map<T,int>& dictionary, std::map<std::pair<T, T>,int>& pairDict, std::pair<T,T>& input);
 
 std::pair<std::string,std::string> getInput(void);
+
+template <typename T>
+void play(std::map<T,int>& dictionary, std::map<std::pair<T, T>,int>& pairDict);
 
 
 int main(int argc, const char * argv[]) {
@@ -65,28 +68,33 @@ int main(int argc, const char * argv[]) {
     }
     
     printDict(wordCount);
-            
-
-    int playing = 1;
-    while(playing == true){
-        auto input = getInput();
-        calcBigramProb(wordCount, pairMap, input);
-        std::cout << "To try another pair, press 1 >";
-        std::cin >> playing;
-        std::cout << std::endl;
-    }
+    
+    play(wordCount, pairMap);
+    
     std::cout << "Goodbye.\n";
     return 0;
 }
 
 
+template <typename T>
+void play(std::map<T,int>& dictionary, std::map<std::pair<T, T>,int>& pairDict){
+    int playing = 1;
+    while(playing == true){
+        auto input = getInput();
+        calcBigramProb(dictionary, pairDict, input);
+        std::cout << "To try another pair, press 1 >";
+        std::cin >> playing;
+        std::cout << std::endl;
+    }
+    return;
+}
 
 
 template <typename T>
-void printDict(std::map<T,int> dictionary){
+void printDict(std::map<T,int>& dictionary){
     std::cout << std::endl << "************" << std::endl;
     std::cout << "\nNow printing list of words...\n";
-    for(typename std::map<T, int>::iterator it = dictionary.begin(); it != dictionary.end(); ++it){
+    for(typename std::map<T,int>::iterator it = dictionary.begin(); it != dictionary.end(); ++it){
         std::cout << it->first << " appears " << it->second << " time(s)"<< std::endl;
     }
     std::cout << std::endl << "************"<< std::endl;
@@ -104,14 +112,16 @@ std::pair<std::string,std::string> getInput(void){
     return std::make_pair(word1, word2);
 }
 
+
 template <typename T>
-void calcBigramProb(std::map<T,int>& dictionary, std::map<std::pair<T, T>,int>& pairDict, std::pair<T,T> input){
+void calcBigramProb(std::map<T,int>& dictionary, std::map<std::pair<T, T>,int>& pairDict, std::pair<T,T>& input){
     T firstWord = input.first;
     T secWord = input.second;
     
+    double bigramProb = 0;
+    
     //if pair appears together, at least 1 instance of 1st word so denom will never equal 0
     //if pair never appears together, num = 0
-    double bigramProb = 0;
     typename std::map<std::pair<T, T>,int>::iterator bigram = pairDict.find(input);
     if(bigram != pairDict.end()){
         double unigramFreq = dictionary[firstWord];
